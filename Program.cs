7237fc1794e52222e71c7fa245d3fe64ac68445e
MATHEUSFERRAZ08 <matheusgabrielferraz0802@gmail.com>
2022-04-08 22:40:15 -0300

    Atividade finalizada

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿int idadeUsuario;
+
+//Espacamento
+Console.WriteLine();
+
+//Solicita e armazena a idade do usuário
+Console.Write("Insira sua idade, em anos: ");
+idadeUsuario = Convert.ToInt32(Console.ReadLine());
+
+//Espacamento
+Console.WriteLine();
+
+//Testando para idade
+if (idadeUsuario < 18)
+{
+    //Mensagem de erro, em vermelho, com som
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Beep();
+
+    Console.WriteLine("Você não pode doar sangue.");
+}
+else if (idadeUsuario > 67)
+{
+    //Mensagem de erro, em vermelho, com som
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Beep();
+
+    Console.WriteLine("Você não pode doar sangue.");
+}
+else
+{
+    //Mensagem de sucesso em verde
+    Console.ForegroundColor = ConsoleColor.Green;
+
+    Console.WriteLine("Você pode doar sangue.");
+}
+
+//Resetando cores
+Console.ResetColor();
+
+//Espacamento
+Console.WriteLine();
